aec7110c61fd580accafd94baf470dc1bccba86b
Dexter Fitch <fitch.dexter@gmail.com>
2020-09-30 17:24:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,9 @@ class Student
 
   def initialize(student_hash)
     student_hash.each do |key, val|
-binding.pry
-      # self[key] = val
+      instance_variable_set("@#{key}", val)
     end
-    # @name = student_hash[:name]
-    # @location = student_hash[:location]
+
     @@all << self
   end
 
